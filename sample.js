739eff225732a36216c89842f3f00a3295ca3bcd
laprn <reg@dymcom.net>
2020-12-03 22:45:10 +0900

    first commit

---
+++ sample.js
@@ -0,0 +1,5 @@
+function hello(name) {
+    console.log('Hello ' + name + '!');
+}
+var your_name = 'TypeScript';
+hello(your_name);
---
+++ sample.ts
@@ -0,0 +1,6 @@
+function hello(name: string): void {
+    console.log('Hello ' + name + '!');
+}
+
+let your_name = 'TypeScript';
+hello(your_name);
